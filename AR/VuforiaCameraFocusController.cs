51da906661115b1b62e1547cfd6c3a3229110c5e
Dragos Silion <silidragos@gmail.com>
2018-09-02 20:28:50 +0300

    Utility Focus camera script for Vuforia

---
+++ AR/VuforiaCameraFocusController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+/*
+Source: https://developer.vuforia.com/forum/unity-extension-technical-discussion/camera-focus-mode-android-unity#comment-57665
+Attach the "CameraFocusController.cs" script to ARCamera
+Used when focus didn't work on some phones when using Vuforia
+*/
+public class VuforiaCameraFocusController : MonoBehaviour {
+
+    private bool mVuforiaStarted = false;
+
+    void Start () 
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if (vuforia != null)
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+    }
+
+    private void StartAfterVuforia()
+    {
+        mVuforiaStarted = true;
+        SetAutofocus();
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            // App resumed
+            if (mVuforiaStarted)
+            {
+                // App resumed and vuforia already started
+                // but lets start it again...
+                SetAutofocus(); // This is done because some android devices lose the auto focus after resume
+                // this was a bug in vuforia 4 and 5. I haven't checked 6, but the code is harmless anyway
+            }
+        }
+    }
+
+    private void SetAutofocus()
+    {
+        if (CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
+        {
+            Debug.Log("Autofocus set");
+        }
+        else
+        {
+            // never actually seen a device that doesn't support this, but just in case
+            Debug.Log("this device doesn't support auto focus");
+        }
+    }
+}
